b837c7f66378c4379a9ef8eab5f40f15170aedf3
idning <idning@gmail.com>
2012-09-23 17:57:23 +0800

    rename

---
+++ common/utils.c
@@ -0,0 +1,23 @@
+/*
+ * file   : test_ptrace.c
+ * author : ning
+ * date   : 2012-01-13 16:10:27
+ */
+
+#include<stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+
+int util_set_max_files(int max_files)
+{
+    struct rlimit rls;
+    rls.rlim_cur = max_files;
+    rls.rlim_max = max_files;
+
+    if (setrlimit(RLIMIT_NOFILE, &rls) < 0) {
+        fprintf(stderr, "can't change open files limit to %u\n", max_files);
+    }
+    return 0;
+}
---
+++ common/utils.h
@@ -0,0 +1,8 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+int util_set_max_files(int max_files);
+
+#endif
+
+
